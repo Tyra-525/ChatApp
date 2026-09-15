564ec6e8a3ccbb5f70f13708d8d971f8b8e46697
Student <Student@LabVM2049939.ivuoocfc5jguli3c4m4ckmwkfe.jnbx.internal.cloudapp.net>
2026-09-16 00:04:16 +0200

    method for name

--- src/main/java/com/mycompany/chatapp/Login.java
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -20,4 +20,14 @@ public String getUsername(){return username;}
 public String getPassword(){return password;}
 public String getCellPhoneNumber(){return cellPhoneNumber;}
 
+//method for name
+public boolean name(String name){ //if theres a problem its because you exlcded the name cannot be empty 
+if (!name.trim().matches("[ a-zA-Z]+")){
+System.out.println("First name must contain letters only");
+return false;
+}
+this.name = name.trim();
+return true;
+}
+
 }
